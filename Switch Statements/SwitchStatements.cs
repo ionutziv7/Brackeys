0d9c83db3418f89a9503e998bbaf02e8fdb40f28
John I <93291968+ionutziv7@users.noreply.github.com>
2022-03-08 14:29:17 +0200

    Update SwitchStatements.cs

--- Switch Statements/SwitchStatements.cs
+++ Switch Statements/SwitchStatements.cs
@@ -11,22 +11,39 @@ namespace SwitchStatements
             int num2 = numberGenerator.Next(1, 11); 
             Console.WriteLine("What is " + num1 + " times " + num2 + "?");
             int answer = Convert.ToInt32(Console.ReadLine());
-            if(answer ==  num1 * num2) {
-                Console.WriteLine("Well done! Your answer is correct. ");
-            } else {
+            if(answer ==  num1 * num2)
+            {
                 int responseindex = numberGenerator.Next(1, 4);
-                switch(responseindex) {
+                switch(responseindex)
+                {
                     case 1:
-                        Console.WriteLine("Are you even trying?");
+                        Console.WriteLine("Correct, well done!");
                         break;
                     case 2:
-                        Console.WriteLine("The answer was incorrect.");
+                        Console.WriteLine("The answer was correct.");
                         break;
                     default:
-                        Console.WriteLine("You can do better than that.");
+                        Console.WriteLine("Have you been practicing ?");
                         break;
                 }
             }
+            else
+            {
+                int diff = Math.Abs(answer - (num1 * num2));
+                if(diff == 1)
+                {
+                    Console.WriteLine("So close");
+                }
+                else if(diff <= 10)
+                {
+                    Console.WriteLine("You can do better than that.");
+                }
+                else
+                {
+                    Console.WriteLine("You are not even close.");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
